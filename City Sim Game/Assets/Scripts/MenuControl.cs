e7e041b3a87bfd52ca2c1065ffb559d3996682b1
karlsvenssonn <B27ojo95>
2020-09-17 09:38:02 +0200

    Created a simple start Menu

---
+++ City Sim Game/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit();
+    }
+}
