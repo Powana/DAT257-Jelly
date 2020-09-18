1e6bc1f810f064b92c999132867d3699309b3d0a
Adam Thunberg <thadam@student.chalmers.se>
2020-09-18 15:33:52 +0200

    Revert "Isometric camera"
    This reverts commit 28c43b8ecc46386425ab6c41bbbdfa250e3c3780.

--- City Sim Game/Assets/Scripts/cameraController.cs
+++ City Sim Game/Assets/Scripts/cameraController.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-    private Camera cam;
-    private float targetZoom;
-    private float zoomFactor = 3f;
-    [SeriallizeField] private float zoomLerpSpeed = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
+	private Camera cam;
+	private float targetZoom;
+	private float zoomFactor = 3f;
+	[SerializeField] private float zoomLerpSpeed = 10;
+	// Start is called before the first frame update
+	void Start()
+	{
 
-        cam = Camera.main;
-        targetZoom = cam.orthographicSize;
+		cam = Camera.main;
+		targetZoom = cam.orthographicSize;
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        float scrollData;
-        scrollData = Input.GetAxis("Mouse ScrollWheel");
-        targetZoom -= scrollData * zoomFactor;
-        targetZoom = Mathf.Clamp(targetZoom, 4.5f, 8f);
-        cam.orthographicSize = Matchf.Lerb(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		float scrollData;
+		scrollData = Input.GetAxis("Mouse ScrollWheel");
+		targetZoom -= scrollData * zoomFactor;
+		targetZoom = Mathf.Clamp(targetZoom, 4.5f, 8f);
+		cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+	}
 }
