732bcc3b4888b8be27c020941b8df6dda5cf7bd0
Adam Thunberg <thadam@student.chalmers.se>
2020-09-22 09:05:03 +0200

    Modified some bounds.

--- City Sim Game/Assets/Scripts/CameraController.cs
+++ City Sim Game/Assets/Scripts/CameraController.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// This class is made to controll camera functions 
+// This class is made to controll camera functions
 /*
- * Camera is the objcet of the main camera 
+ * Camera is the objcet of the main camera
  * targetZoom is the size of the camera
  * zoomFactor is to controll zoom strength
  * zoomlerpSpeed is to controll zoom speed
@@ -15,13 +15,12 @@ public class CameraController : MonoBehaviour
 	private float targetZoom;
 	private float zoomFactor = 3f;
 	[SerializeField] private float zoomLerpSpeed = 10;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
 		cam = Camera.main;
 		targetZoom = cam.orthographicSize;
-
 	}
 
 	// Update is called once per frame
@@ -30,7 +29,9 @@ public class CameraController : MonoBehaviour
 		float scrollData;
 		scrollData = Input.GetAxis("Mouse ScrollWheel");
 		targetZoom -= scrollData * zoomFactor;
-		targetZoom = Mathf.Clamp(targetZoom, 4.5f, 8f);
+
+		// The parameters specify the size bounds.
+		targetZoom = Mathf.Clamp(targetZoom, 1f, 8f);
 		cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
 	}
 }
