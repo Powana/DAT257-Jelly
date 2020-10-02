15d104c57018de75915abc0f01a94c07c9a715c4
Ben <benw@student.chalmers.se>
2020-10-02 16:55:44 +0200

    Replaced old road sprites
    For some reason the layer ordering is now wrong, that's a todo

--- City Sim Game/Assets/Scripts/Cells/Road.cs
+++ City Sim Game/Assets/Scripts/Cells/Road.cs
@@ -16,10 +16,6 @@ public class Road : Cell
         GameObject go = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Basic/Road"));
         SpriteRenderer roadSpriteR = go.GetComponentInChildren<SpriteRenderer>();
 
-        // Reset flips
-        roadSpriteR.flipX = false;
-        roadSpriteR.flipY = false;
-
         // Check surrounding tiles for connecting roads
         bool north = tilemap.GetTile(position + new Vector3Int(1, 0, 0)) is Road;
         bool east = tilemap.GetTile(position + new Vector3Int(0, -1, 0)) is Road;
@@ -28,18 +24,20 @@ public class Road : Cell
 
         bool[] connected = { north, east, south, west };
 
+        roadSpriteR.flipX = false;
+        roadSpriteR.flipY = false;
 
         // I like dis
         switch (connected.Count(x => x))
         {
             // No roads connected
             case 0:
-                spritePath = "road_tile_none";
+                spritePath = "road__tile_none";
                 break;
 
             // Dead end
             case 1:
-                spritePath = "road_tile_S";
+                spritePath = "road__tile_S";
                 if (east)       { roadSpriteR.flipX = true; }
                 else if (west)  { roadSpriteR.flipY = true; }
                 else if (north) { roadSpriteR.flipX = true;
@@ -48,21 +46,21 @@ public class Road : Cell
 
             case 2:
                 // Assume N/S straight road
-                spritePath = "road_tile_NS";
+                spritePath = "road__tile_NS";
                 if (east && west) { roadSpriteR.flipX = true; }
                 // If not straight
-                else if (north && east) { spritePath = "road_tile_NE"; }
-                else if (south && west) { spritePath = "road_tile_NE";
+                else if (north && east) { spritePath = "road__tile_NE"; }
+                else if (south && west) { spritePath = "road__tile_NE";
                                           roadSpriteR.flipX = true; }
 
-                else if (north && west) { spritePath = "road_tile_NW"; }
-                else if (south && east) { spritePath = "road_tile_NW";
+                else if (north && west) { spritePath = "road__tile_NW"; }
+                else if (south && east) { spritePath = "road__tile_NW";
                                           roadSpriteR.flipY = true; }
                 break;
 
             // T-junction
             case 3:
-                spritePath = "road_tile_NEW";
+                spritePath = "road__tile_NEW";
                 if (!north)      { roadSpriteR.flipX = true;
                                    roadSpriteR.flipY = true; }
                 else if (!east)  { roadSpriteR.flipX = true; }
@@ -71,7 +69,7 @@ public class Road : Cell
 
             // 4-way junction
             case 4:
-                spritePath = "road_tile_NESW";
+                spritePath = "road__tile_NESW";
                 break;
         }
         roadSpriteR.sprite = Resources.Load<Sprite>("Sprites/roads/" + spritePath);
