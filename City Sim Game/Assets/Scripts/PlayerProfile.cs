f4acc37958165f16744bfaf4ce279de79587d820
karlsvenssonn <Svensson.Karl@icloud.com>
2020-10-07 06:17:14 +0200

    New scripts for saving a game

---
+++ City Sim Game/Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProfile
+{
+    public string playerName;
+    public int money;
+    public int pollution;
+    public int food;
+    public int energy;
+}
---
+++ City Sim Game/Assets/Scripts/SaveData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData 
+{
+    private static SaveData _current;
+    private static SaveData current
+    {
+        get
+        {
+            if(_current == null)
+            {
+                _current = new SaveData();
+            }
+
+            return _current;
+        }
+    }
+
+    public PlayerProfile profile;
+}
